6f5bd39462f5d02ea598e797b603d950e947be7b
shrishti chaurasia <131389624+ShrishtiChaurasia@users.noreply.github.com>
2023-10-02 09:12:54 +0100

    JS tooltip
    JS tippy tooltip.

---
+++ Tooltip/tooltip.js
@@ -0,0 +1,13 @@
+
+tippy('#myButton', {
+    content: "I'm a Tippy tooltip!",
+    placement: 'top',
+    arrow: true,
+    animation: 'scale-extreme',
+    theme: 'translucent',
+    interactive: true,
+    delay: 500, // ms
+    followCursor: true,
+  });
+  
+  // you can add more properties with the help of tippy JS
